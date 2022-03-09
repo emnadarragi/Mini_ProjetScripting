3063c20f0f996df9486e239a51009de9cb128882
AlaBelgacem <alaaeddine.belgacem@esprit.tn>
2022-03-09 19:01:18 +0100

    Final commit

--- inforep.sh
+++ inforep.sh
@@ -32,6 +32,7 @@ AfficheNbD
 echo -e " "
 echo "Nombre de fihciers : "
 AfficheNbF
+echo -e " "
 ;;
 2 )TypeFIle ;;
 3 ) TypeFile;;
@@ -39,7 +40,7 @@ AfficheNbF
 5 ) Menu_Graph
 ;;
 6 ) ;;
-7 );;
+7 )auteurs;;
 8 )AccessFiles
 echo -e " "
 PropFiles ;;
@@ -56,7 +57,11 @@ done
 
 
 
-
+function auteurs() {
+  echo -e "Auteurs : Emna Darragi et Alaa Eddine Belgacem."
+  echo -e " "
+  echo -e "Version 2.0"
+}
 
 function AfficheNbF(){
 
@@ -83,6 +88,7 @@ function AccessFiles() { #display access rights with file name
 
     #ls -l | grep ^- | awk '{print $1,  $9}'
     #ls -l | cut -f 1 -d '.'
+    cd ~/Desktop/$OPTARG
     ls -l | grep ^- | awk '{print" Nom Du Fichier : " $9, " || Droit D accès : "  $1}'
     #ls -l | grep ^- | cut -d ' ' -f1,12
 
@@ -90,6 +96,7 @@ function AccessFiles() { #display access rights with file name
 
 function PropFiles(){
   #ls -l | grep ^- | cut -d ' ' -f3,12
+  cd ~/Desktop/$OPTARG
   ls -l | grep ^- | awk '{print" Nom Du Fichier : " $9, " || Proprietaire : "  $3}'
 }
 
@@ -145,10 +152,12 @@ function TypeFile()
 }
 
 function AccessFile(){
+  cd ~/Desktop;
   ls -l ${OPTARG} | awk '{print "Droit D accès : "  $1}'
 }
 
 function PropFile() {
+  cd ~/Desktop;
   ls -l ${OPTARG} | awk '{print "Proprietaire : "  $3, " || Groupe : " $4}'
 }
 
@@ -176,7 +185,23 @@ function PropFile() {
 function Menu_Graph() #affichage d'un menu graphique avec l'option YAD
 {
 
-YAD_OPTIONS="--window-icon='dialog-information' --name=IxSysinfo"
+  champs=$(yad --width=400 --height=400 --title="Donner un dossier ou un fichier" --text-align="center" \
+--form \
+--field="Dossier:" '' \
+--field="Fichier:" '' )
+echo "$champs"
+#Récupération des champs~/Desktop/${champ1} avec awk.
+champ1=$(echo $champs | awk -F'|' '{print $1}')
+champ2=$(echo $champs | awk -F'|' '{ print $2 }')
+#champ3=$(echo $champs | awk 'BEGIN {FS="|" } { print $3 }')
+#Affichage des champs.
+echo "$champ1"
+#echo "$champ2"
+
+
+
+
+YAD_OPTIONS="--window-icon='dialog-information' --name=IxGestion"
 
      KEY=$RANDOM
 
@@ -189,37 +214,37 @@ YAD_OPTIONS="--window-icon='dialog-information' --name=IxSysinfo"
     export -f show_mod_info
 
 
-    # Memory tab
-    sed -r "s/:[ ]*/\n/" /proc/meminfo |\
-        yad --plug=$KEY --tabnum=1 --image=memory --text=$"Memory usage information" \
-            --list --no-selection --column=$"Parameter" --column=$"Value" &
+    # Files tab
+    ls -l ~/Desktop/${champ2} | awk '{printf "%s\n%s\n%s\n%s\n", $9,$1,$3,$4}' |\
+        yad --plug=$KEY --tabnum=1 --image=drive-harddisk --text=$"Informations sur le fichier en argument" \
+            --list --no-selection --column=$"Fichier" --column=$"Droits" --column=$"Proprietaire" --column=$"Groupe" &
 
-    # Harddrive tab
-    ls -l | grep ^- | awk '{printf "%s\n%s\n%s\n", $9,$1,$3}' |\
-    #df -T | tail -n +2 | awk '{printf "%s\n%s\n%s\n%s\n%s\n%s\n", $1,$7, $2, $3, $4, $6}' |\
-        yad --plug=$KEY --tabnum=2 --image=drive-harddisk --text=$"Disk space usage" \
+    # Repos tab
+    ls -l ~/Desktop/${champ1} | grep ^- | awk '{printf "%s\n%s\n%s\n", $9,$1,$3}' |\
+
+        yad --plug=$KEY --tabnum=2 --image=drive-harddisk --text=$"Informations sur les fichiers dans ce repertoires" \
             --list --no-selection --column=$"Nom" --column=$"Droits" --column=$"Proprietaire" &
 
 
     # help tab
 sed -r "s/:[ ]*/\n/" ~/Desktop/HELP.txt |\
-        yad --plug=$KEY --tabnum=3 --image=help --text=$"Help space usage" \
+        yad --plug=$KEY --tabnum=3 --image=help --text=$"Help" \
             --text-info =$"content" &
 
     # Auteurs tab
 
-        yad --plug=$KEY --tabnum=4 --image=help --text=$" Authors : Alaa Eddine Belgacem et Emna Darragi \n Version : 1.0" &
+        yad --plug=$KEY --tabnum=4 --image=help --text=$" Authors : Alaa Eddine Belgacem et Emna Darragi \n Version : 2.0" &
 
 
 
     # main dialog
-    TXT=$"<b>Informations sur les répertoires</b>\\n\\n"
+    TXT=$"<b>Informations sur les répertoires et les fichiers</b>\\n\\n"
     TXT+=$"\\tOS: $(lsb_release -ds) on $(hostname)\\n\\n"
     TXT+=$"\\tVersion de noyau: $(uname -mr)\\n\\n"
     TXT+="\\t<i>$(uptime)</i>"
 
-    yad --notebook --width=1200 --height=500 --title=$"System info" --text="$TXT" --button="close" \
-        --key=$KEY  --tab=$"Dossiers" --tab=$"Fichiers" --tab=$"Help" --tab=$"Auteurs"   \
+    yad --notebook --width=1200 --height=500 --title=$"Gestion des repertoires et fichiers" --text="$TXT" --button="close" \
+        --key=$KEY  --tab=$"Fichiers" --tab=$"Dossiers" --tab=$"Help" --tab=$"Auteurs"   \
           --active-tab=${1:-1}
 
 }
@@ -237,10 +262,12 @@ HELP
 show_usage ;
 if [ $# -gt 0 ]; then
 
-while getopts "hT:t:Aa:N:n:gm:s:E:" var
+while getopts "hvT:t:A:a:N:n:gm:s:E:" var
 do
 case $var in
 h)HELP
+;;
+v)auteurs
 ;;
 T)TypeFIle
 ;;
